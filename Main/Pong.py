fd576849c2aff201d82dcaf0492b00b7375e3174
iExploze <54289660+iExploze@users.noreply.github.com>
2021-07-31 17:53:42 -0400

    Updated the paddles
    Made two paddles and made the screen alot bigger.

--- Main/Pong.py
+++ Main/Pong.py
@@ -3,7 +3,7 @@ import turtle
 window = turtle.Screen()
 window.title("Pong for learning")
 window.bgcolor("black")
-window.setup(width=800, height=600)
+window.setup(width=1920, height=1080)
 window.tracer(0)
 
 # Paddle Left
@@ -11,12 +11,19 @@ paddle_left = turtle.Turtle()
 paddle_left.speed(0)
 paddle_left.shape("square")
 paddle_left.color("White")
+paddle_left.shapesize(stretch_wid=10, stretch_len=2)
 paddle_left.penup()
-paddle_left.goto(-350, 0)
+paddle_left.goto(-800, 0)
 
 
 # Paddle Right
-
+paddle_right = turtle.Turtle()
+paddle_right.speed(0)
+paddle_right.shape("square")
+paddle_right.color("White")
+paddle_right.shapesize(stretch_wid=10, stretch_len=2)
+paddle_right.penup()
+paddle_right.goto(800, 0)
 
 # Ball
 
