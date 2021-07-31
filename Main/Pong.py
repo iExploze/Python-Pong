10960cc26d45a5b0e2def4919a0665e81e4d9fff
iExploze <54289660+iExploze@users.noreply.github.com>
2021-07-31 17:49:25 -0400

    made a paddle

--- Main/Pong.py
+++ Main/Pong.py
@@ -6,6 +6,21 @@ window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)
 
+# Paddle Left
+paddle_left = turtle.Turtle()
+paddle_left.speed(0)
+paddle_left.shape("square")
+paddle_left.color("White")
+paddle_left.penup()
+paddle_left.goto(-350, 0)
+
+
+# Paddle Right
+
+
+# Ball
+
+
 # main game loop
 while True:
     window.update()
