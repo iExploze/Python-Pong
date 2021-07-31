203170b6e7fd77714be904cb7b7f9b202dc23421
iExploze <54289660+iExploze@users.noreply.github.com>
2021-07-31 17:46:14 -0400

    Made the window

--- Main/Pong.py
+++ Main/Pong.py
@@ -1,2 +1,11 @@
 import turtle
 
+window = turtle.Screen()
+window.title("Pong for learning")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+# main game loop
+while True:
+    window.update()
